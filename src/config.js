c9a3d7629b52789218ddd3d9ef9da9b7eb976fb7
Me <me@Mes-MacBook-Pro.local>
2021-07-12 17:05:49 -0700

    recheck

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  PORT: 9000,
-  NODE_ENV: "development",
-  CLIENT_ORIGIN: 3000,
-  DATABASE_URL: "postgresql://localhost/migraine_app",
-  TEST_DATABASE_URL: "postgresql://localhost/migraine_app_test",
-  JWT_SECRET: "change-this-secret"
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 3000,
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://localhost/migraine_app",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || "postgresql://localhost/migraine_app_test",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
 };
 
 const { expect } = require("chai");
